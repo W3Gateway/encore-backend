6c2df7892be3cb8080d6e58060dfd64416dc343c
Paulo Cedro <pdehon.cc@gmail.com>
2023-07-25 15:35:17 -0300

    Alteração nome repositório otherquestion

---
+++ src/Encore.Application/Questions/OtherQuestionResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Encore.Application.Questions
+{
+    public class OtherQuestionResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public OtherQuestionResponse(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
