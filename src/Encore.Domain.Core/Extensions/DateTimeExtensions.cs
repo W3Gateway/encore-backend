441d0ca64a4748fe225c25ed6e22e663d613ea1d
Paulo Cedro <pdehon.cc@gmail.com>
2024-03-09 15:54:46 -0300

    adicionada extensão epoch pra data

---
+++ src/Encore.Domain.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Encore.Domain.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToEpoch(this DateTime dateTime)
+        {
+            DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(dateTime.ToUniversalTime() - epochStart).TotalSeconds;
+        }
+    }
+}
